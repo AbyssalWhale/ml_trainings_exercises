af89fdc5a8a37755bd090c66f522463131bf10a6
olehh <>
2025-08-17 14:15:08 +0200

    adding load and showing lab5 raw images

--- labs/lab5.py
+++ labs/lab5.py
@@ -1,6 +1,10 @@
 import logging
+import os.path
 
 from tools.device import get_device, get_model
+from tools.helper_system import get_lab_data_path
+from PIL import Image
+import matplotlib.pyplot as plt
 
 
 def lab5():
@@ -11,7 +15,28 @@ def lab5():
         logging.info("LAB4. PREPARATION")
         device = get_device()
         model = get_model(name="lab4_model.pth", device=device)
+
+        logging.info("preparing images for inference")
+        path_image_a, path_image_b = get_and_show_lab_images()
+
         pass
     except (RuntimeError, ValueError, TypeError) as e:
         logging.error("Error in lab5: %s", e)
         raise
+
+def get_and_show_lab_images() -> tuple:
+    path_image_a = get_lab_data_path(lab_name="lab5", item_name="a.png")
+    path_image_b = get_lab_data_path(lab_name="lab5", item_name="b.png")
+
+    fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+    axes[0].imshow(Image.open(path_image_a), cmap='gray')
+    axes[0].set_title("Image means letter: A")
+    axes[0].axis('off')
+
+    axes[1].imshow(Image.open(path_image_b), cmap='gray')
+    axes[1].set_title("Image means letter: B")
+    axes[1].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+    return path_image_a, path_image_b
